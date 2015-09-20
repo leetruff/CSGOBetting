37340fbf9fdae19422d78791bf31167df5f8f339
Lars <leetruff@gmail.com>
2015-09-20 23:48:04 +0200

    MergeTest 2.2

--- CSGOBetting/src/Launcher.java
+++ CSGOBetting/src/Launcher.java
@@ -2,7 +2,7 @@
 public class Launcher {
 	public static void main(String[] args){
 		System.out.println("LOLAAAAAAAAAAAAA");
-		System.out.println("abc");
+		System.out.println("sfs");
 		System.out.println("Penis");
 		System.out.println("Penis22");
 	}
