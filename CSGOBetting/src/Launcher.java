c4783d53f84923a27e6dab62c223f43b2d169315
TobiasM95 <tobias.mielich@freenet.de>
2015-09-20 23:48:06 +0200

    Merge Test 2

--- CSGOBetting/src/Launcher.java
+++ CSGOBetting/src/Launcher.java
@@ -2,7 +2,6 @@
 public class Launcher {
 	public static void main(String[] args){
 		System.out.println("LOLAAAAAAAAAAAAA");
-		System.out.println("abc");
 		System.out.println("Penis");
 		System.out.println("Penis22");
 	}
